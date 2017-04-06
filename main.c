239444f0f0e8c7d67fc2e137ee2f95cc4f614d11
kmchmk1026 <kmchmk@gmail.com>
2017-04-07 00:21:06 +0530

    member and insert functions are added. Delete is not implemented yes

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ struct node{
 
 bool member(struct node* head, int value){
     struct node* tempNode = head;
-    while(tempNode->next != NULL){
+    while(tempNode != NULL){
         if(tempNode->data == value){
             return true;
         }
@@ -30,21 +30,51 @@ void insert(struct node* head,int value){
 }
 
 
+bool delete(struct node* head, int value){
+    struct node* previouseTempNode = head;
+    struct node* middleTempNode = previouseTempNode->next;
+    struct node* nextTempNode = middleTempNode->next;
+    while(middleTempNode != NULL){
+        previouseTempNode = previouseTempNode->next;
+        middleTempNode = previouseTempNode->next;
+        nextTempNode = middleTempNode->next;
+        if(middleTempNode->data == value){
+            previouseTempNode->next = nextTempNode;
+            return true;
+        }
+    }
+    return false;
+}
+
 
 void main(){
 
 	struct node* head = malloc(sizeof(node));
 	head->data = 11;
-    insert(head, 34);
-    insert(head,23);
-
-    if(member(head,21)){
+    insert(head,22);
+    insert(head,33);
+    insert(head,44);
+    insert(head,55);
+    insert(head,66);
+    insert(head,77);
+    insert(head,88);
+    insert(head,99);
+    if(member(head,66)){
         printf("member\n");
     }
     else{
         printf("not member\n");
     }
 
-	printf("head value is ");
-	printf("%d",head->next->next->data);
+    delete(head,64);
+
+    if(member(head,66)){
+        printf("member\n");
+    }
+    else{
+        printf("not member\n");
+    }
+
+	//printf("head value is ");
+	//printf("%d",head->next->next->data);
 }
