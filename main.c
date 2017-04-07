fb4fb99dfd3f2109ea7c95396ebafe33182d84cb
kmchmk1026 <kmchmk@gmail.com>
2017-04-07 14:57:41 +0530

    Delete function, random number generator, and input n,m added

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<stdbool.h>
+#include<math.h>
 
 struct node{
 	int data;
 	struct node* next;
 }node;
+
+struct node* create(){
+    struct node* head = NULL;
+    return head;
+}
 
 bool member(struct node* head, int value){
     struct node* tempNode = head;
@@ -30,49 +36,95 @@ void insert(struct node* head,int value){
 }
 
 
-bool delete(struct node* head, int value){
-    struct node* previouseTempNode = head;
-    struct node* middleTempNode = previouseTempNode->next;
-    struct node* nextTempNode = middleTempNode->next;
-    while(middleTempNode != NULL){
-        previouseTempNode = previouseTempNode->next;
-        middleTempNode = previouseTempNode->next;
-        nextTempNode = middleTempNode->next;
-        if(middleTempNode->data == value){
-            previouseTempNode->next = nextTempNode;
-            return true;
+struct node* delete(struct node* head, int value){
+    if(head->data == value){
+        return head->next;
+    }
+    else{
+        struct node* previouseTempNode = head;
+        struct node* middleTempNode = previouseTempNode->next;
+        while(middleTempNode != NULL){
+            struct node* nextTempNode = middleTempNode->next;
+            if(middleTempNode->data == value){
+                previouseTempNode->next = nextTempNode;
+                return head;
+            }
+            if(nextTempNode == NULL){
+                return head;
+            }
+            previouseTempNode = previouseTempNode->next;
+            middleTempNode = previouseTempNode->next;
+            nextTempNode = middleTempNode->next;
         }
     }
-    return false;
+    return head;
 }
 
+int* randomNumber(int howMany){
+    int max = pow(2,16);
+    int numberArray[max];
+    for (int i = 0; i < max; i++){
+        numberArray[i] = i;
+    }
+    srand(time(NULL));
+    for (int i = 0; i < max; i++){
+        int randNum = rand()%(max);
+        int temp = numberArray[i];
+        numberArray[i] = numberArray[randNum];
+        numberArray[randNum] = temp;
+    }
+
+    int* randomArray = malloc(howMany);
+
+    for(int i =0; i < howMany; i++){
+        randomArray[i] = numberArray[i];
+        //printf("%d ", randomArray[i]);
+    }
+
+    return randomArray;
+
+}
 
 void main(){
 
-	struct node* head = malloc(sizeof(node));
-	head->data = 11;
-    insert(head,22);
-    insert(head,33);
-    insert(head,44);
-    insert(head,55);
-    insert(head,66);
-    insert(head,77);
-    insert(head,88);
-    insert(head,99);
-    if(member(head,66)){
-        printf("member\n");
-    }
-    else{
-        printf("not member\n");
+    int n, m;
+    float m_member, m_insert, m_delete;
+    printf("Input \"n\": ");
+    scanf("%d",&n);
+    printf("Input \"m\": ");
+    scanf("%d",&m);
+    printf("Enter fractions:\n");
+    printf("\tMember: ");
+    scanf("%f",&m_member);
+    printf("\tInsert: ");
+    scanf("%f",&m_insert);
+    printf("\tDelete: ");
+    scanf("%f",&m_delete);
+
+    int numberOfMember = m * m_member;
+    int numberOfInsert = m * m_insert;
+    int numberOfDelete = m * m_delete;
+
+    printf("%d\n",numberOfMember);
+    printf("%d\n",numberOfInsert);
+    printf("%d\n",numberOfDelete);
+
+    if(1 != (int)(m_member + m_insert + m_delete)){
+        printf("\nSum of fractions should be 1.\n");
+        return;
     }
 
-    delete(head,64);
+    //printf("%f\n",m_member + m_insert + m_delete);
+    int *randomArray = randomNumber(n);
 
-    if(member(head,66)){
-        printf("member\n");
-    }
-    else{
-        printf("not member\n");
+    int functionArray[m];
+
+
+
+
+    struct node* head = create();
+    for(int i = 0; i < n; i++){
+        printf("%d ", randomArray[i]);
     }
 
 	//printf("head value is ");
