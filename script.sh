125e9d4121d253c6e5e12d478ce79c8e465ff56b
sameeraw <smrwijeratna@gmail.com>
2017-04-09 11:37:38 +0530

    edited script file again

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@
 rm serial_case_1.txt
 rm serial_case_2.txt
 rm serial_case_3.txt
+
 rm mutex_case_1_thread_1.txt
 rm mutex_case_1_thread_2.txt
 rm mutex_case_1_thread_4.txt
@@ -15,9 +16,23 @@ rm mutex_case_3_thread_2.txt
 rm mutex_case_3_thread_4.txt
 rm mutex_case_3_thread_8.txt
 
+rm locks_case_1_thread_1.txt
+rm locks_case_1_thread_2.txt
+rm locks_case_1_thread_4.txt
+rm locks_case_1_thread_8.txt
+rm locks_case_2_thread_1.txt
+rm locks_case_2_thread_2.txt
+rm locks_case_2_thread_4.txt
+rm locks_case_2_thread_8.txt
+rm locks_case_3_thread_1.txt
+rm locks_case_3_thread_2.txt
+rm locks_case_3_thread_4.txt
+rm locks_case_3_thread_8.txt
+
 gcc serial_case_1.c -o serial_case_1
 gcc serial_case_2.c -o serial_case_2
 gcc serial_case_3.c -o serial_case_3
+
 gcc mutex_case_1_thread_1.c -o mutex_case_1_thread_1
 gcc mutex_case_1_thread_2.c -o mutex_case_1_thread_2
 gcc mutex_case_1_thread_4.c -o mutex_case_1_thread_4
@@ -30,6 +45,19 @@ gcc mutex_case_3_thread_1.c -o mutex_case_3_thread_1
 gcc mutex_case_3_thread_2.c -o mutex_case_3_thread_2
 gcc mutex_case_3_thread_4.c -o mutex_case_3_thread_4
 gcc mutex_case_3_thread_8.c -o mutex_case_3_thread_8
+
+gcc locks_case_1_thread_1.c -o locks_case_1_thread_1
+gcc locks_case_1_thread_2.c -o locks_case_1_thread_2
+gcc locks_case_1_thread_4.c -o locks_case_1_thread_4
+gcc locks_case_1_thread_8.c -o locks_case_1_thread_8
+gcc locks_case_2_thread_1.c -o locks_case_2_thread_1
+gcc locks_case_2_thread_2.c -o locks_case_2_thread_2
+gcc locks_case_2_thread_4.c -o locks_case_2_thread_4
+gcc locks_case_2_thread_8.c -o locks_case_2_thread_8
+gcc locks_case_3_thread_1.c -o locks_case_3_thread_1
+gcc locks_case_3_thread_2.c -o locks_case_3_thread_2
+gcc locks_case_3_thread_4.c -o locks_case_3_thread_4
+gcc locks_case_3_thread_8.c -o locks_case_3_thread_8
 
 for (( c=1; c<=100; c++ ))
 do  
